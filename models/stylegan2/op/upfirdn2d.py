ec9f329c9ae735a42b46b012e591f52f26cf7a1a
HassanMuhammadSannaullah <scientisthassan@gmail.com>
2025-02-10 16:26:16 +0100

    update

--- models/stylegan2/op/upfirdn2d.py
+++ models/stylegan2/op/upfirdn2d.py
@@ -7,13 +7,18 @@ from torch.utils.cpp_extension import load
 
 
 module_path = os.path.dirname(__file__)
-upfirdn2d_op = load(
-    "upfirdn2d",
-    sources=[
-        os.path.join(module_path, "upfirdn2d.cpp"),
-        os.path.join(module_path, "upfirdn2d_kernel.cu"),
-    ],
-)
+
+# Check if CUDA is available
+if torch.cuda.is_available():
+    upfirdn2d_op = load(
+        "upfirdn2d",
+        sources=[
+            os.path.join(module_path, "upfirdn2d.cpp"),
+            os.path.join(module_path, "upfirdn2d_kernel.cu"),
+        ],
+    )
+else:
+    upfirdn2d_op = None
 
 
 class UpFirDn2dBackward(Function):
@@ -21,26 +26,28 @@ class UpFirDn2dBackward(Function):
     def forward(
         ctx, grad_output, kernel, grad_kernel, up, down, pad, g_pad, in_size, out_size
     ):
+        if upfirdn2d_op is not None:
+            up_x, up_y = up
+            down_x, down_y = down
+            g_pad_x0, g_pad_x1, g_pad_y0, g_pad_y1 = g_pad
 
-        up_x, up_y = up
-        down_x, down_y = down
-        g_pad_x0, g_pad_x1, g_pad_y0, g_pad_y1 = g_pad
+            grad_output = grad_output.reshape(-1, out_size[0], out_size[1], 1)
 
-        grad_output = grad_output.reshape(-1, out_size[0], out_size[1], 1)
-
-        grad_input = upfirdn2d_op.upfirdn2d(
-            grad_output,
-            grad_kernel,
-            down_x,
-            down_y,
-            up_x,
-            up_y,
-            g_pad_x0,
-            g_pad_x1,
-            g_pad_y0,
-            g_pad_y1,
-        )
-        grad_input = grad_input.view(in_size[0], in_size[1], in_size[2], in_size[3])
+            grad_input = upfirdn2d_op.upfirdn2d(
+                grad_output,
+                grad_kernel,
+                down_x,
+                down_y,
+                up_x,
+                up_y,
+                g_pad_x0,
+                g_pad_x1,
+                g_pad_y0,
+                g_pad_y1,
+            )
+            grad_input = grad_input.view(in_size[0], in_size[1], in_size[2], in_size[3])
+        else:
+            grad_input = F.leaky_relu(grad_output, negative_slope=0.2) * scale
 
         ctx.save_for_backward(kernel)
 
@@ -63,24 +70,26 @@ class UpFirDn2dBackward(Function):
     def backward(ctx, gradgrad_input):
         (kernel,) = ctx.saved_tensors
 
-        gradgrad_input = gradgrad_input.reshape(-1, ctx.in_size[2], ctx.in_size[3], 1)
+        if upfirdn2d_op is not None:
+            gradgrad_input = gradgrad_input.reshape(-1, ctx.in_size[2], ctx.in_size[3], 1)
 
-        gradgrad_out = upfirdn2d_op.upfirdn2d(
-            gradgrad_input,
-            kernel,
-            ctx.up_x,
-            ctx.up_y,
-            ctx.down_x,
-            ctx.down_y,
-            ctx.pad_x0,
-            ctx.pad_x1,
-            ctx.pad_y0,
-            ctx.pad_y1,
-        )
-        # gradgrad_out = gradgrad_out.view(ctx.in_size[0], ctx.out_size[0], ctx.out_size[1], ctx.in_size[3])
-        gradgrad_out = gradgrad_out.view(
-            ctx.in_size[0], ctx.in_size[1], ctx.out_size[0], ctx.out_size[1]
-        )
+            gradgrad_out = upfirdn2d_op.upfirdn2d(
+                gradgrad_input,
+                kernel,
+                ctx.up_x,
+                ctx.up_y,
+                ctx.down_x,
+                ctx.down_y,
+                ctx.pad_x0,
+                ctx.pad_x1,
+                ctx.pad_y0,
+                ctx.pad_y1,
+            )
+            gradgrad_out = gradgrad_out.view(
+                ctx.in_size[0], ctx.in_size[1], ctx.out_size[0], ctx.out_size[1]
+            )
+        else:
+            gradgrad_out = F.leaky_relu(gradgrad_input, negative_slope=0.2) * scale
 
         return gradgrad_out, None, None, None, None, None, None, None, None
 
@@ -88,38 +97,40 @@ class UpFirDn2dBackward(Function):
 class UpFirDn2d(Function):
     @staticmethod
     def forward(ctx, input, kernel, up, down, pad):
-        up_x, up_y = up
-        down_x, down_y = down
-        pad_x0, pad_x1, pad_y0, pad_y1 = pad
+        if upfirdn2d_op is not None:
+            up_x, up_y = up
+            down_x, down_y = down
+            pad_x0, pad_x1, pad_y0, pad_y1 = pad
 
-        kernel_h, kernel_w = kernel.shape
-        batch, channel, in_h, in_w = input.shape
-        ctx.in_size = input.shape
+            kernel_h, kernel_w = kernel.shape
+            batch, channel, in_h, in_w = input.shape
+            ctx.in_size = input.shape
 
-        input = input.reshape(-1, in_h, in_w, 1)
+            input = input.reshape(-1, in_h, in_w, 1)
 
-        ctx.save_for_backward(kernel, torch.flip(kernel, [0, 1]))
+            ctx.save_for_backward(kernel, torch.flip(kernel, [0, 1]))
 
-        out_h = (in_h * up_y + pad_y0 + pad_y1 - kernel_h) // down_y + 1
-        out_w = (in_w * up_x + pad_x0 + pad_x1 - kernel_w) // down_x + 1
-        ctx.out_size = (out_h, out_w)
+            out_h = (in_h * up_y + pad_y0 + pad_y1 - kernel_h) // down_y + 1
+            out_w = (in_w * up_x + pad_x0 + pad_x1 - kernel_w) // down_x + 1
+            ctx.out_size = (out_h, out_w)
 
-        ctx.up = (up_x, up_y)
-        ctx.down = (down_x, down_y)
-        ctx.pad = (pad_x0, pad_x1, pad_y0, pad_y1)
+            ctx.up = (up_x, up_y)
+            ctx.down = (down_x, down_y)
+            ctx.pad = (pad_x0, pad_x1, pad_y0, pad_y1)
 
-        g_pad_x0 = kernel_w - pad_x0 - 1
-        g_pad_y0 = kernel_h - pad_y0 - 1
-        g_pad_x1 = in_w * up_x - out_w * down_x + pad_x0 - up_x + 1
-        g_pad_y1 = in_h * up_y - out_h * down_y + pad_y0 - up_y + 1
+            g_pad_x0 = kernel_w - pad_x0 - 1
+            g_pad_y0 = kernel_h - pad_y0 - 1
+            g_pad_x1 = in_w * up_x - out_w * down_x + pad_x0 - up_x + 1
+            g_pad_y1 = in_h * up_y - out_h * down_y + pad_y0 - up_y + 1
 
-        ctx.g_pad = (g_pad_x0, g_pad_x1, g_pad_y0, g_pad_y1)
+            ctx.g_pad = (g_pad_x0, g_pad_x1, g_pad_y0, g_pad_y1)
 
-        out = upfirdn2d_op.upfirdn2d(
-            input, kernel, up_x, up_y, down_x, down_y, pad_x0, pad_x1, pad_y0, pad_y1
-        )
-        # out = out.view(major, out_h, out_w, minor)
-        out = out.view(-1, channel, out_h, out_w)
+            out = upfirdn2d_op.upfirdn2d(
+                input, kernel, up_x, up_y, down_x, down_y, pad_x0, pad_x1, pad_y0, pad_y1
+            )
+            out = out.view(-1, channel, out_h, out_w)
+        else:
+            out = F.leaky_relu(input + kernel, negative_slope=0.2) * scale
 
         return out
 
@@ -143,7 +154,7 @@ class UpFirDn2d(Function):
 
 
 def upfirdn2d(input, kernel, up=1, down=1, pad=(0, 0)):
-    if input.device.type == "cpu":
+    if input.device.type == "cpu" or upfirdn2d_op is None:
         out = upfirdn2d_native(
             input, kernel, up, up, down, down, pad[0], pad[1], pad[0], pad[1]
         )
